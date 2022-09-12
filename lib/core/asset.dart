599e3d8cfd17faafb3dfa1557b9f06b0855eae73
paola-poffo <paola.poffo@warren.com.br>
2022-09-12 17:56:11 -0300

    Feat : finishing the app

---
+++ lib/core/asset.dart
@@ -0,0 +1,5 @@
+const String btc = 'assets/images/BTC.png';
+const String eth = 'assets/images/ETH.png';
+const String ltc = 'assets/images/LTC.png';
+const String movements = 'assets/images/movementsOut.png';
+const String warren = 'assets/images/warrenOn.png';
