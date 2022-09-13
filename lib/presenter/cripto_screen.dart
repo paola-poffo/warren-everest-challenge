9f7b0a1a2a089f2d374ed6b024d2c57a17f50ddb
paola-poffo <paola.poffo@warren.com.br>
2022-09-13 14:03:56 -0300

    Feat :componentizing some items

--- lib/presenter/cripto_screen.dart
+++ lib/presenter/cripto_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'widgets/visible_button.dart';
+import '../shared/widgets/bottom_navigation.dart';
 import '../shared/providers/providers.dart';
 import '../core/asset.dart';
 import 'widgets/cripto_type.dart';
@@ -14,134 +16,96 @@ class CriptoScreen extends StatefulHookConsumerWidget {
 }
 
 class _CriptoScreenState extends ConsumerState<CriptoScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     var stateVisible = ref.watch(visibilityProvider.state);
 
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(right: 10, left: 20),
-                padding: const EdgeInsets.only(top: 10, bottom: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Cripto",
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 10, left: 20),
+              padding: const EdgeInsets.only(top: 10, bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Cripto",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                          color: const Color.fromRGBO(224, 43, 87, 1),
+                        ),
+                      ),
+                      const VisibleButton(),
+                    ],
+                  ),
+                  stateVisible.state
+                      ? Text(
+                          NumberFormat.simpleCurrency(locale: 'pt-BR')
+                              .format(14798.00),
                           style: GoogleFonts.montserrat(
                             fontSize: 32,
                             fontWeight: FontWeight.w700,
-                            color: const Color.fromRGBO(224, 43, 87, 1),
+                          ),
+                        )
+                      : Container(
+                          width: 210,
+                          height: 39,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(8),
                           ),
                         ),
-                        IconButton(
-                          onPressed: () {
-                            setState(
-                              () {
-                                stateVisible.state = !stateVisible.state;
-                              },
-                            );
-                          },
-                          icon: Icon(stateVisible.state
-                              ? Icons.remove_red_eye
-                              : Icons.visibility_off),
-                        ),
-                      ],
+                  const SizedBox(height: 6),
+                  const Text(
+                    "Valor total de moedas",
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Color.fromRGBO(117, 118, 128, 1),
+                      fontWeight: FontWeight.w400,
                     ),
-                    stateVisible.state
-                        ? Text(
-                            NumberFormat.simpleCurrency(locale: 'pt-BR').format(14798.00),
-                            style: GoogleFonts.montserrat(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          )
-                        : Container(
-                            width: 210,
-                            height: 39,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                    const SizedBox(height: 6),
-                    const Text(
-                      "Valor total de moedas",
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Color.fromRGBO(117, 118, 128, 1),
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const Divider(
-                thickness: 1,
+            ),
+            Expanded(
+              child: ListView(
+                children: const [
+                  CriptoType(
+                    name: 'Bitcoin',
+                    abbreviation: 'BTC',
+                    value: 6557.00,
+                    done: 0.65,
+                    image: btc,
+                  ),
+                  CriptoType(
+                    name: 'Ethereum',
+                    abbreviation: 'ETH',
+                    value: 7996.00,
+                    done: 0.94,
+                    image: eth,
+                  ),
+                  CriptoType(
+                    name: 'Litecoin',
+                    abbreviation: 'LTC',
+                    value: 245.00,
+                    done: 0.82,
+                    image: ltc,
+                  ),
+                ],
               ),
-              const CriptoType(
-                name: 'Bitcoin',
-                abbreviation: 'BTC',
-                value: 6557.00,
-                done: 0.65,
-                image: btc,
-              ),
-              const Divider(
-                thickness: 1,
-              ),
-              const CriptoType(
-                name: 'Ethereum',
-                abbreviation: 'ETH',
-                value: 7996.00,
-                done: 0.94,
-                image: eth,
-              ),
-              const Divider(
-                thickness: 1,
-              ),
-              const CriptoType(
-                name: 'Litecoin',
-                abbreviation: 'LTC',
-                value: 245.00,
-                done: 0.82,
-                image: ltc,
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(warren),
-                ),
-                label: 'Portifólio'),
-            BottomNavigationBarItem(
-                icon: ImageIcon(
-                  AssetImage(movements),
-                ),
-                label: 'Movimentações'),
+            ),
           ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color.fromRGBO(224, 43, 87, 1),
-          onTap: _onItemTapped,
         ),
+        bottomNavigationBar: const BottomNavigation(),
       ),
     );
+
+
   }
 }
--- lib/presenter/widgets/cripto_type.dart
+++ lib/presenter/widgets/cripto_type.dart
@@ -29,109 +29,115 @@ class _CriptoTypeState extends ConsumerState<CriptoType> {
   Widget build(BuildContext context) {
     final visible = ref.watch(visibilityProvider.state);
 
-    return Container(
-      margin: const EdgeInsets.only(top: 8, bottom: 8),
-      padding: const EdgeInsets.only(left: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+    return Column(
+      children: [
+        const Divider(thickness: 1),
+        Container(
+          margin: const EdgeInsets.only(top: 8, bottom: 8),
+          padding: const EdgeInsets.only(left: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CircleAvatar(
-                radius: 26,
-                backgroundImage: AssetImage(widget.image),
-              ),
-              const SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    widget.abbreviation,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 19,
-                    ),
+                  CircleAvatar(
+                    radius: 26,
+                    backgroundImage: AssetImage(widget.image),
                   ),
-                  const SizedBox(height: 6),
-                  Text(
-                    widget.name,
-                    style: const TextStyle(
-                      color: Color.fromRGBO(117, 118, 128, 1),
-                      fontSize: 15,
-                    ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.abbreviation,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 19,
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      Text(
+                        widget.name,
+                        style: const TextStyle(
+                          color: Color.fromRGBO(117, 118, 128, 1),
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-          Row(
-            children: [
-              visible.state
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          NumberFormat.simpleCurrency(locale: 'pt-BR').format(widget.value),
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                        const SizedBox(height: 8),
-                        Row(
+              Row(
+                children: [
+                  visible.state
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
                             Text(
-                              widget.done.toString(),
-                              style: const TextStyle(
-                                color: Color.fromRGBO(117, 118, 128, 1),
-                                fontSize: 15,
+                              NumberFormat.simpleCurrency(locale: 'pt-BR')
+                                  .format(widget.value),
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            const SizedBox(height: 8),
+                            Row(
+                              children: [
+                                Text(
+                                  widget.done.toString(),
+                                  style: const TextStyle(
+                                    color: Color.fromRGBO(117, 118, 128, 1),
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const SizedBox(width: 5),
+                                Text(
+                                  widget.abbreviation,
+                                  style: const TextStyle(
+                                    color: Color.fromRGBO(117, 118, 128, 1),
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 110,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade200,
+                                borderRadius: BorderRadius.circular(8),
                               ),
                             ),
-                            const SizedBox(width: 5),
-                            Text(
-                              widget.abbreviation,
-                              style: const TextStyle(
-                                color: Color.fromRGBO(117, 118, 128, 1),
-                                fontSize: 15,
+                            const SizedBox(height: 8),
+                            Container(
+                              width: 80,
+                              height: 18,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade200,
+                                borderRadius: BorderRadius.circular(8),
                               ),
                             ),
                           ],
-                        )
-                      ],
-                    )
-                  : Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Container(
-                          width: 110,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade200,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
                         ),
-                        const SizedBox(height: 8),
-                        Container(
-                          width: 80,
-                          height: 18,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade200,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                      ],
-                    ),
-              Column(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_forward_ios_rounded),
-                    onPressed: () {},
-                    color: const Color.fromRGBO(117, 118, 128, 1),
-                    iconSize: 18,
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        onPressed: () {},
+                        color: const Color.fromRGBO(117, 118, 128, 1),
+                        iconSize: 18,
+                      ),
+                    ],
                   ),
                 ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
