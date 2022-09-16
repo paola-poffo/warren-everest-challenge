c420a32ed6b9bf0f87ae84b21acdb99f9471d963
paola-poffo <paola.poffo@warren.com.br>
2022-09-16 11:30:52 -0300

    Feat: adding the graphic

--- lib/details/widgets/graphic.dart
+++ lib/details/widgets/graphic.dart
@@ -1,22 +1,46 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class Graphic extends StatefulWidget {
+class Graphic extends StatelessWidget {
   const Graphic({Key? key}) : super(key: key);
 
   @override
-  State<Graphic> createState() => _GraphicState();
-}
-
-class _GraphicState extends State<Graphic> {
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: Container(
-        height: 200,
-        width: 400,
-        decoration: const BoxDecoration(
-          color: Colors.grey,
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(
+              barWidth: 3.5,
+              color: const Color.fromRGBO(224, 43, 87, 1),
+              dotData: FlDotData(show: false),
+              spots: [],
+            ),
+          ],
+          betweenBarsData: [],
+          titlesData: FlTitlesData(show: false),
+          extraLinesData: ExtraLinesData(),
+          lineTouchData: LineTouchData(enabled: true),
+          rangeAnnotations: RangeAnnotations(),
+          showingTooltipIndicators: [],
+          gridData: FlGridData(show: false),
+          borderData: FlBorderData(
+            border: const Border(
+              bottom: BorderSide(
+                width: 3,
+                color: Color.fromRGBO(227, 228, 235, 1),
+              ),
+            ),
+          ),
+          minX: 0,
+          maxX: 10,
+          baselineX: 3,
+          minY: 0,
+          maxY: 15,
+          baselineY: 3,
+          clipData: FlClipData.none(),
+          backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
         ),
       ),
     );
