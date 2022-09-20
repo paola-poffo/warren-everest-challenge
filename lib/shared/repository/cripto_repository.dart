b4b86a5f3e75d6aa8bf1926d2e7e119b667f5bc3
paola-poffo <113135466+paola-poffo@users.noreply.github.com>
2022-09-20 17:16:45 -0300

    Fix: testing the API

--- lib/shared/repository/cripto_repository.dart
+++ lib/shared/repository/cripto_repository.dart
@@ -8,8 +8,8 @@ class CriptoRepository {
   Future<List<CriptoModels>> getAllCoins() async {
     List<CriptoModels> criptos = [];
 
-    final response = await dio.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=market_cap_desc&per_page=10&page=1&sparkline=false
-');
+    final response = await dio.get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=BRL&order=market_cap_desc&per_page=10&page=1&sparkline=false');
 
     criptos = List.from(
       response.data.map((cripto) {
