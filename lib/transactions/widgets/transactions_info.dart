3a21b19390b814a177bc51147ef5eac5b8b285d5
paola-poffo <113135466+paola-poffo@users.noreply.github.com>
2022-10-13 09:54:37 -0300

    Fix: fixing the converted symbol

--- lib/transactions/widgets/transactions_info.dart
+++ lib/transactions/widgets/transactions_info.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class TransactionsInfo extends StatelessWidget {
+import '../../conversion/provider/single_cripto_provider.dart';
+
+class TransactionsInfo extends ConsumerWidget {
   final String convertQuantity;
   final String receiveQuantity;
   final String total;
   final DateTime date;
+
   const TransactionsInfo({
     Key? key,
     required this.convertQuantity,
@@ -15,7 +19,9 @@ class TransactionsInfo extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final convertedCripto = ref.watch(convertedCriptoProvider.state);
+
     return ListTile(
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -25,7 +31,7 @@ class TransactionsInfo extends StatelessWidget {
             style: TextStyle(color: Colors.grey.shade700),
           ),
           Text(
-            receiveQuantity,
+            '$receiveQuantity ${convertedCripto.state.symbol.toUpperCase()}',
             style: const TextStyle(
               fontSize: 17,
               fontWeight: FontWeight.w400,
